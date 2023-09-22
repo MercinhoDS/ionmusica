c89011b1f44d9f4d44c41759bbe14d256ed39cd9
Mercinho <mercinhodosantos@gmail.com>
2023-09-22 15:14:00 -0300

    22.09

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD8SxC4SgW4RrfaIGSIwXRU7URphY2YReE",
+    authDomain: "ionmusica-702aa.firebaseapp.com",
+    projectId: "ionmusica-702aa",
+    storageBucket: "ionmusica-702aa.appspot.com",
+    messagingSenderId: "829877297136",
+    appId: "1:829877297136:web:63b990c8e0f335e757c735"
+  }
 };
