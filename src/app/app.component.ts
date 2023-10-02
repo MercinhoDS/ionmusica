0e19704bfb60f9d01416aed52c0a77242ce19cc5
MercinhoDS <mercinhodosantos@gmail.com>
2023-10-02 16:11:16 -0300

    02.10.2023_finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { environment } from 'src/environments/environment';
+
+// Importa o Core do Firebase.
+import { initializeApp } from 'firebase/app';
+
+// Importe apenas os componentes que forem necessários.
+import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +16,54 @@ import { environment } from 'src/environments/environment';
 // Menu principal.
 export class AppComponent {
 
+  // Inicializa core do Firebase.
+  app = initializeApp(environment.firebase);
+
+  // Inicializa Authentication.
+  auth = getAuth(this.app);
+
   // Variáveis de ambiente.
   env = environment;
 
+  // Dados do usuário na view, quando não logado.
+  appUser = {
+    logged: false,
+    title: 'Login / Entrar',
+    url: '/login',
+    icon: 'log-in',
+    avatar: ''
+  }
+
+  // Menu principal.
   public appPages = [
     { title: 'Início', url: 'home', icon: 'home' },
     { title: 'Contatos', url: 'contacts', icon: 'mail' },
     { title: 'Sobre', url: 'about', icon: 'information-circle' },
     { title: 'Autores', url: 'author', icon: 'people' },
-    { title: 'Privacidade', url: 'polices', icon: 'lock-closed' }
+    { title: 'Privacidade', url: 'policies', icon: 'lock-closed' }
   ];
 
   constructor() { }
+
+  ngOnInit() {
+
+    // Monitora status do usuário.
+    onAuthStateChanged(this.auth, (userData) => {
+
+      if (userData) {
+
+        // Dados do usuário na view, quando logado.
+        this.appUser = {
+          logged: true,
+          title: userData.displayName + '',
+          url: '/profile',
+          icon: 'log-out',
+          avatar: userData.photoURL + ''
+        }
+
+      }
+
+    });
+  }
+
 }
